9df3935f5bd22aaa87dc37e9e3b286b3deec6524
Brian <thehoots26@gmail.com>
2021-07-14 14:35:09 -0500

    Assignment Complete

--- queries.sql
+++ queries.sql
@@ -11,3 +11,6 @@ SELECT name FROM employer WHERE employer.location = "St. Louis City";
 DROP TABLE job;
 
 ## Part 4: Test it with SQL
+SELECT skill.name, skill.description FROM skill
+    INNER JOIN job_skills ON skill.id = job_skills.skills_id
+    ORDER BY name ASC;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,8 +1,10 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
@@ -11,12 +13,23 @@ public class Skill extends AbstractEntity {
     @Size(min = 5, max = 250)
     private String description;
 
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs;
+
     public String getDescription() {
         return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
     }
 
     public Skill() {}
